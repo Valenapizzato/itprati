14e48b656e61d029a18d93fcc91e53423f9d096f
Valenapizzato <valenapizzato@gmail.com>
2025-04-08 11:53:13 -0300

    Corrigido host e porta para Render

--- app.py
+++ app.py
@@ -83,5 +83,6 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
